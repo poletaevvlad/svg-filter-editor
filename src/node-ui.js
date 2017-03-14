c8ad58c01fc2a979f39eded3765e2970c597b1be
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-03-14 21:10:42 +0300

    Added inputs and outputs to node

--- src/node-ui.js
+++ src/node-ui.js
@@ -13,6 +13,22 @@ class Node extends React.Component{
 	render(){
 		return <div className="node" style={{left: `${this.props.left}px`, top: `${this.props.top}px`}}>
 			<div className="header" onMouseDown={this._handleDragMouseDown}>{this.title}</div>
+			<div className="io">
+				<div className="inputs">
+				{this.props.primitive.inputs.map(input => {
+					return <div className="io-component" key={this.props.primitive.getInputId(input)}>
+					{input.title}
+					<div className="io-connector" id={this.props.primitive.getInputId(input)} /></div>
+				})}
+				</div>
+				<div className="outputs">
+				{this.props.primitive.outputs.map(output => {
+					return <div className="io-component" key={this.props.primitive.getOutputId(output)}>
+					{output.title}
+					<div className="io-connector" id={this.props.primitive.getOutputId(output)} /></div>
+				})}
+				</div>
+			</div>
 		</div>
 	}
 
--- src/primitive.js
+++ src/primitive.js
@@ -5,14 +5,33 @@ function generateId(){
 	return ++lastId;
 }
 
+function input(title, id){
+	return {title: title, id: id};
+}
+
+function output(title, id){
+	return {title: title, id: id};
+}
+
 class Primitive{
 	constructor(){
 		this.id = generateId();
+
+		this.inputs = [input("Input 1", 1), input("Input 2", 2)];
+		this.outputs = [output("Output 1", 1), output("Output 2", 2), output("Output 3", 3)];
 
 		this.positionX = 50;
 		this.positionY = 40;
 
 		this.nodeComponentClass = Node;
+	}
+
+	getInputId(inp){
+		return `pr${this.id}_in${inp.id}`;
+	}
+
+	getOutputId(out){
+		return `pr${this.id}_out${out.id}`;
 	}
 }
 
