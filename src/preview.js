2a9d42bbb0892bd128ff00ea22ad7e36eb59a66c
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-03-25 19:11:51 +0300

    Implemented a way to edit the background of preview area

--- src/preview.js
+++ src/preview.js
@@ -8,6 +8,11 @@ class Preview extends React.Component{
 		}else{
 			return <svg id="preview-root">
 				<defs>
+					<pattern id="checkerboard" patternUnits="userSpaceOnUse" width="18" height="18" className={"checkerboard " + this.props.bgCheckerboard}>
+						<rect className="light" x="0" y="0" width="18" height="18" />
+						<rect className="dark" x="0" y="0" width="9" height="9" />
+						<rect className="dark" x="9" y="9" width="9" height="9" />
+					</pattern>
 					<filter id="filter" width="1000" height="1000" x="-500" y="-500">
 						{primitives.map(primitive => {
 							let PrimitiveComponent = primitive.svgComponentClass;
@@ -16,11 +21,24 @@ class Preview extends React.Component{
 						})}
 					</filter>
 				</defs>
+				<rect x="0" y="0" width="100%" height="100%" {... this._getBackground()} />
 				<g filter="url(#filter)">
 					<circle fill="orange" r="30" cx="50%" cy="50%" />
 				</g>
 			</svg>
 		}
+	}
+
+	_getBackground(){
+		switch(this.props.bgType){
+			case "transparent": 
+				return {fill: "transparent"}
+			case "color":
+				return {fill: this.props.bgColor}
+			case "checkerboard":
+				return {fill: "url(#checkerboard)"}
+		}
+		return {};
 	}
 }
 
--- src/root.js
+++ src/root.js
@@ -3,6 +3,45 @@ import React from "react";
 import NodesContainer from "./nodes-container.js";
 import Preview from "./preview.js";
 import Filter from "./filter.js";
+import ComboBox from "./components/combobox.js";
+import ColorPicker from "./components/color-picker.js";
+import Selector from "./components/selector.js";
+
+
+class BackgroundEditor extends React.Component{
+	constructor(){
+		super();
+		this.types = [
+			{value: "transparent", label: "Transparent"},
+			{value: "color", label: "Solid color"},
+			{value: "checkerboard", label: "Checkerboard"}
+		]
+
+		this.checkerboardTypes = [
+			{value: "dark", "class": "checkerboard-dark"},
+			{value: "light", "class": "checkerboard-light"},
+			{value: "contrast", "class": "checkerboard-contrast"}
+		]
+	}
+
+	render(){
+		return <div>
+			<ComboBox value={this.props.type} width={200 - 18} values={this.types} 
+				label="background type:" onChange={this.props.onTypeChange} />
+			{ this.props.type == "color" ? 
+				<div className="content">
+					<ColorPicker color={this.props.color} onChange={this.props.onColorChanged}/>
+				</div>
+			: this.props.type == "checkerboard" ?
+				<div className="content">
+					<Selector values={this.checkerboardTypes} value={this.props.checkerboard} 
+						onChange={this.props.onCheckerboardChanged}/>
+				</div>
+			: null
+			}
+		</div>
+	}
+}
 
 class FilterEditor extends React.Component{
 	constructor(){
@@ -17,8 +56,12 @@ class FilterEditor extends React.Component{
 		this._separatorX = 0;
 
 		this.state = {
-			previewWidth: 400
+			previewWidth: 400,
+			bgType: "transparent",
+			bgColor: "black",
+			bgCheckerboard: "dark"
 		}
+
 	}
 
 	componentWillMount(){
@@ -36,8 +79,21 @@ class FilterEditor extends React.Component{
 			<NodesContainer filter={this.filter} onUpdate={this._handleUpdate} right={this.state.previewWidth} />
 			<div id="result-preview" style={{width: `${this.state.previewWidth}px`}}>
 				<div id="separator" onMouseDown={this._separatorMouseDown}></div>
-				<div id="toolbar"></div>
-				<Preview filter={this.filter} />
+				<div id="toolbar">
+					<div className="section">Shape <div className="dropdown-icon" /></div>
+					<div className="section">Background <div className="dropdown-icon" /></div>
+				</div>
+
+				<div id="preview-editor">
+					<BackgroundEditor type={this.state.bgType} color={this.state.bgColor}
+						width={this.state.previewWidth} checkerboard={this.state.bgCheckerboard}
+						onTypeChange={val => this.setState({bgType: val})} 
+						onColorChanged={val => this.setState({bgColor: val.hex})}
+						onCheckerboardChanged={val => this.setState({bgCheckerboard: val})} />
+				</div>
+
+				<Preview filter={this.filter} bgType={this.state.bgType} 
+					bgColor={this.state.bgColor} bgCheckerboard={this.state.bgCheckerboard} />
 			</div>
 		</div>
 	}
