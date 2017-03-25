5b836c5dbefa14c0145fca92427ffa928e0c1d13
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-03-25 19:11:08 +0300

    Implemented <Selector /> React component

---
+++ src/components/selector.js
@@ -0,0 +1,17 @@
+import React from "react";
+import focused from "./focused.js";
+
+class Selector extends React.Component{
+	render(){
+		return <div className="selector">
+			{this.props.values.map(val => {
+					let value = val.value;
+					return <div key={val.value} onClick={e => {focused(); this.props.onChange(value)}}
+						className={"item " + val.class + (this.props.value == val.value ? " selected" : "")} />
+				}
+			)}
+		</div>
+	}
+}
+
+module.exports = Selector;
