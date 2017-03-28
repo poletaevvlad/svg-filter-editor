d5b5fc11cb7270bf68db294d12d45c28f6b61a9e
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-03-28 10:53:43 +0300

    Not displaying input/output bloc of primitive component if empty

--- src/node-ui.js
+++ src/node-ui.js
@@ -14,24 +14,30 @@ class Node extends React.Component{
 			style={{left: `${this.props.left}px`, top: `${this.props.top}px`, width: `${this.props.primitive.nodeWidth}px`}}>
 			<div className={"header" + (this.props.dragging? " dragging": "")} onMouseDown={this._handleDragMouseDown}>{this.title}</div>
 			<div className="io">
-				<div className="inputs">
-				{this.props.primitive.inputs.map(input => {
-					return <div className="io-component" key={this.props.primitive.getInputId(input)}>
-					{input.title}
-					<div className={this._getConnectorClass(input.id, "input")} id={this.props.primitive.getInputId(input)} 
-						data-primitiveid={this.props.primitive.id}
-						data-iotype="input" data-ioid={input.id} /></div>
-				})}
-				</div>
-				<div className="outputs">
-				{this.props.primitive.outputs.map(output => {
-					return <div className="io-component" key={this.props.primitive.getOutputId(output)}>
-					{output.title}
-					<div className={this._getConnectorClass(output.id, "output")} id={this.props.primitive.getOutputId(output)} 
-						data-primitiveid={this.props.primitive.id}
-						data-iotype="output" data-ioid={output.id} /></div>
-				})}
-				</div>
+				{this.props.primitive.inputs.length == 0 ? null :
+					<div className="inputs">
+					{this.props.primitive.inputs.map(input => {
+						return <div className="io-component" key={this.props.primitive.getInputId(input)}>
+						{input.title}
+						<div className={this._getConnectorClass(input.id, "input")} 
+							id={this.props.primitive.getInputId(input)} 
+							data-primitiveid={this.props.primitive.id}
+							data-iotype="input" data-ioid={input.id} /></div>
+					})}
+					</div>
+				}
+				{this.props.primitive.outputs.length == 0 ? null : 
+					<div className="outputs">
+					{this.props.primitive.outputs.map(output => {
+						return <div className="io-component" key={this.props.primitive.getOutputId(output)}>
+						{output.title}
+						<div className={this._getConnectorClass(output.id, "output")} 
+							id={this.props.primitive.getOutputId(output)}
+							data-primitiveid={this.props.primitive.id}
+							data-iotype="output" data-ioid={output.id} /></div>
+					})}
+					</div>
+				}
 			</div>
 			<div className="content">{this.renderEditor()}</div>
 		</div>
