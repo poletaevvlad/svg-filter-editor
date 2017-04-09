4a35f90fafd27ebc7a0bb7a6966c88973a79f2c3
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-04-09 15:19:47 +0300

    Displaying real svg attributes in code instead of React's camel case variants

--- src/code-display.js
+++ src/code-display.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+let REACT_ATTRIBUTES={
+	"xlinkHref": "xlink:href",
+	"floodColor": "flood-color",
+	"floodOpacity": "flood-opacity",
+	"lightingColor": "lighting-color"
+}
+
 class CodeDisplay extends React.Component{
 	render(){
 		let lineIndex = 1;
@@ -19,6 +26,10 @@ class CodeDisplay extends React.Component{
 		</div>;
 	}
 
+	getAttribute(attr){
+		return REACT_ATTRIBUTES[attr] || attr;
+	}
+
 	makeLine(lines, tag, offset){
 		if (tag == null){
 			return;
@@ -26,7 +37,7 @@ class CodeDisplay extends React.Component{
 
 		let args = []
 		for (let arg in tag.args){
-			args.push(<span key={arg}> <span className="attr">{arg}</span>=<span className="attr-value">&quot;{tag.args[arg]}&quot;</span></span>);
+			args.push(<span key={arg}> <span className="attr">{this.getAttribute(arg)}</span>=<span className="attr-value">&quot;{tag.args[arg]}&quot;</span></span>);
 		}
 
 		lines.push(<div key={lines.length} className="code">
